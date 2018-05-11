0ce4db2df3647b1936f309ed3896404c057608c2
Vsevolod <vv.rymar@MacBook-Pro-Rymar.local>
2018-05-11 16:48:48 +0300

    MMC > Screenshot

---
+++ screenshot.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# cd ~/Desktop/
+# cd ~/Documents/GitHub/android-bash
+# bash screenshot.sh
+
+
+echo 
+echo ……………………………… SCREENSHOTS
+echo ~
+cd ~/Documents/GitHub/android-bash
+
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+filepath=/sdcard/screenshot_$timestamp.png
+echo $timestamp
+echo $filepath
+
+adb shell screencap -p $filepath
+adb pull $filepath ~/Desktop/screenshot_$timestamp.png
+adb shell rm $filepath
+echo ………………………………………………………………
+echo 
